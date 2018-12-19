e3ca95e1aa555d8b1dbc4c13be0fd4282d83aff4
Piotr <piotr.pikul@student.uj.edu.pl>
2018-12-19 20:20:34 +0100

    react-table

--- src/pages/LogWindow.js
+++ src/pages/LogWindow.js
@@ -8,7 +8,6 @@ class LogWindow extends Component {
     return (
       <div>
         <h2>Logowanie</h2>
-        <p>TODO.</p>
         <form id="userdata">
           <div className="form-group"> 
             <input type="checkbox" id="register" className="form-check-input"
@@ -17,20 +16,18 @@ class LogWindow extends Component {
                 this.setState({command:"Załóż konto"});
               else
                 this.setState({command:"Zaloguj się"}); 
-                }}
-            ></input>
+                }} />
             <label htmlFor="register">Nie mam konta</label>
           </div>
           
           <div className="form-group">
             <label htmlFor="username">Login</label>
-            <input className="form-control" id="username"></input>
+            <input className="form-control" id="username" />
           </div>
           <div className="form-group">
             <label htmlFor="pass">Hasło</label>
-            <input type="password" className="form-control" id="pass"></input>
+            <input type="password" className="form-control" id="pass" />
           </div>
-          
           <input type="submit" className="btn" formMethod="post" value={this.state.command} />
         </form>
       </div>
--- src/pages/StatWindow.js
+++ src/pages/StatWindow.js
@@ -1,12 +1,60 @@
 import React, { Component } from "react";
+import axios from "axios";
+import ReactTable from "react-table";
+import 'react-table/react-table.css'
 //import '../App.css';
  
-class StatWindow extends Component {
-  render() {
+class StatWindow extends Component
+{
+  header1 =[
+    { Header: "Plansza", accessor: 'id', resizable: false, width: 70 },
+    { Header: "Znanych rozwiązań", accessor: 'solutions' },
+    { Header: "Min. liczba ruchów", accessor: 'moves' ,
+        Cell: el => <span className="number">{el.value}</span>},
+    { Header: "Liczba graczy",  accessor: "players" }
+    //{ Header: "Średnio ruchów", id: "mean", accessor: (el => el.moves) },
+  ];
+  header2 =[
+    { Header: "Plansza", accessor: 'id', resizable: false, width: 70 },
+    { Header: "Ukończona", id: "solved", accessor: (el => {
+      if (el.player.solved) return "tak";
+      else return "nie";}) },
+    { Header: "Min. liczba ruchów", accessor: 'player.moves' ,
+        Cell: el => <span className="number">{el.value}</span>},
+  ];
+  state= {data :[]};
+
+  componentDidMount() // Docelowo serwer będzie zwracał coś ciekawszego (dynamicznie)
+  {
+    axios.get('/stats.json').then( res=> {
+      this.setState({data: res.data});
+    });
+  }
+
+  render()
+  {
     return (
       <div>
         <h2>Statystyki</h2>
-        <p>TODO.</p>
+        <div>
+          <h3>Ogólne informacje o planszach</h3>
+          <ReactTable defaultPageSize={10} columns={this.header1} data={this.state.data}
+           previousText='Poprzednia' nextText='Następna'
+           loadingText='Czekaj...' className="-striped -highlight"
+           // noDataText='Brak danych'
+           pageText='Strona' ofText='z' rowsText='wierszy' />
+        </div>
+        <div>
+          <h3>Twoje wyniki</h3>
+          <ReactTable defaultPageSize={10} columns={this.header2} data={this.state.data}
+           previousText='Poprzednia'
+           nextText='Następna'
+           loadingText='Czekaj...'
+           // noDataText='Brak danych'
+           pageText='Strona'
+           ofText='z'
+           rowsText='wierszy' />
+        </div>
       </div>
     );
   }
